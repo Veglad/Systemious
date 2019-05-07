11f24d717854e0d82b9622554009adc0c7fe0937
Vladyslav <rek0ra921@gmail.com>
2019-05-07 19:09:17 +0300

    bottom_navigation > Implement Jetpack navigation with bottomNavigation

--- app/src/main/java/com/example/systemious/ui/SystemStateActivity.kt
+++ app/src/main/java/com/example/systemious/ui/SystemStateActivity.kt
@@ -3,33 +3,32 @@ package com.example.systemious.ui
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.systemious.R
 import kotlinx.android.synthetic.main.activity_system_state.*
 
 class SystemStateActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_system_state)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        setSupportActionBar(mainToolbar)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        navController = Navigation.findNavController(this, R.id.mainFragment)
+        bottomNavigation.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
