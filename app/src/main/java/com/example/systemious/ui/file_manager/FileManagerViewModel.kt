c4b220835f3f9da3186d22314e071661c35dc924
Vladyslav <rek0ra921@gmail.com>
2019-05-07 14:27:52 +0300

    bottom_navigation > Add Jetpack Navigation component dependencies

--- app/src/main/java/com/example/systemious/ui/file_manager/FileManagerViewModel.kt
+++ app/src/main/java/com/example/systemious/ui/file_manager/FileManagerViewModel.kt
@@ -1,6 +1,6 @@
 package com.example.systemious.ui.file_manager
 
-import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModel
 
 class FileManagerViewModel : ViewModel() {
     // TODO: Implement the ViewModel
--- app/src/main/java/com/example/systemious/ui/system_details/SystemDetailsViewModel.kt
+++ app/src/main/java/com/example/systemious/ui/system_details/SystemDetailsViewModel.kt
@@ -1,6 +1,6 @@
 package com.example.systemious.ui.system_details
 
-import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModel
 
 class SystemDetailsViewModel : ViewModel() {
     // TODO: Implement the ViewModel
