8a8cf5152e3c9939a5923b535f8d9b4e004e746e
vld_dlv <rek0ra921@gmail.com>
2019-05-09 13:01:38 +0300

    system_info > Add Application class, init system info notification channel

---
+++ app/src/main/java/com/example/systemious/App.kt
@@ -0,0 +1,27 @@
+package com.example.systemious
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import com.example.systemious.utils.Constants
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        createSystemInfoNotificationChannel()
+    }
+
+    private fun createSystemInfoNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceInfoChannel = NotificationChannel(
+                Constants.SERVICE_INFO_NOTIFICATION_CHANNEL_ID,
+                Constants.SERVICE_INFO_NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceInfoChannel)
+        }
+    }
+}
