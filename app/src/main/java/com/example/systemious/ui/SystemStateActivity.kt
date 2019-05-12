48f8bcc547d556f486b0992b653ce817d166106a
vld_dlv <rek0ra921@gmail.com>
2019-05-12 13:36:09 +0300

    system_info > Add start/stop service button

--- app/src/main/java/com/example/systemious/ui/SystemStateActivity.kt
+++ app/src/main/java/com/example/systemious/ui/SystemStateActivity.kt
@@ -7,11 +7,14 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.systemious.R
 import kotlinx.android.synthetic.main.activity_system_state.*
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+
 
 class SystemStateActivity : AppCompatActivity() {
 
@@ -26,10 +29,6 @@ class SystemStateActivity : AppCompatActivity() {
 
         setSupportActionBar(mainToolbar)
         initNavigation()
-
-        viewModel.isSystemServiceWorking.observe(this, Observer<Boolean> { isInfoServiceWorking ->
-           Log.d("Activity", "Isworking: $isInfoServiceWorking")
-        })
     }
 
     private fun initNavigation() {
@@ -50,6 +49,24 @@ class SystemStateActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.navServiceStateToggle -> viewModel.toggleServiceState()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        viewModel.isSystemServiceWorking.observe(this, Observer<Boolean> { isInfoServiceWorking ->
+            val iconId = if (isInfoServiceWorking) R.drawable.ic_stop_secondary_color_24dp else R.drawable.ic_resume_secondary_24dp
+            menu.getItem(0)?.icon = ContextCompat.getDrawable(this, iconId)
+        })
+
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
