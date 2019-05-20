50327f34a116528a8870fae7b4c801033e196a36
Vladyslav <rek0ra921@gmail.com>
2019-05-20 21:18:19 +0300

    app_manager > Finish app manager

--- app/src/main/java/com/example/systemious/ui/app_manager/AppManager.kt
+++ app/src/main/java/com/example/systemious/ui/app_manager/AppManager.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.systemious.R
+import kotlinx.android.synthetic.main.app_manager_fragment.*
 
 class AppManager : Fragment() {
 
@@ -16,6 +19,7 @@ class AppManager : Fragment() {
     }
 
     private lateinit var viewModel: AppManagerViewModel
+    private lateinit var appInfoAdapter: AppManagerRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +31,27 @@ class AppManager : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(AppManagerViewModel::class.java)
-        // TODO: Use the ViewModel
+        initViewModels()
+        initRecyclerView()
+        appManagerSwipeRefresh.setOnRefreshListener { viewModel.loadAppInfo() }
     }
 
+    private fun initViewModels() {
+        viewModel.isLoading.observe(this, Observer { isLoading ->
+            appManagerSwipeRefresh.isRefreshing = isLoading
+        })
+        viewModel.appsInfoList.observe(this, Observer { appsInfoList ->
+            appInfoAdapter.updateAppInfoList(appsInfoList)
+        })
+    }
+
+    private fun initRecyclerView() {
+        activity?.baseContext?.let {context ->
+            appInfoAdapter = AppManagerRecyclerAdapter(context)
+            with(appManagerRecyclerView) {
+                adapter = appInfoAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/systemious/ui/app_manager/AppManagerViewModel.kt
+++ app/src/main/java/com/example/systemious/ui/app_manager/AppManagerViewModel.kt
@@ -1,7 +1,66 @@
 package com.example.systemious.ui.app_manager
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.systemious.App
+import com.example.systemious.ui.app_manager.entities.AppInfo
+import kotlinx.coroutines.*
+import java.io.File
 
-class AppManagerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AppManagerViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val job: Job = Job()
+    private val uiCoroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main + job)
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+
+    private val _appsInfoList = MutableLiveData<MutableList<AppInfo>>()
+    val appsInfoList: LiveData<MutableList<AppInfo>>
+        get() = _appsInfoList
+
+    init {
+        loadAppInfo()
+    }
+
+    fun loadAppInfo() {
+        uiCoroutineScope.launch {
+            _isLoading.value = true
+            try {
+                _appsInfoList.value = withContext(Dispatchers.Default) {
+                    val packageManager = getApplication<App>().packageManager
+                    val appsInflist = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+
+                    val appInfoList = mutableListOf<AppInfo>()
+                    for (appInfo in appsInflist) {
+                        AppInfo().apply {
+                            icon = appInfo.loadIcon(packageManager)
+                            appName = appInfo.loadLabel(packageManager).toString()
+                            packageName = appInfo.packageName
+                            val appSizeInBytes = getApkSize(getApplication(), appInfo.packageName)
+                            sizeInMb = appSizeInBytes.toDouble() / 1048576
+                            appInfoList.add(this)
+                        }
+                    }
+                    appInfoList
+                }
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
+
+    private fun getApkSize(context: Context, packageName: String): Long {
+        return File(context.packageManager.getApplicationInfo(packageName, 0).publicSourceDir).length()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
 }
